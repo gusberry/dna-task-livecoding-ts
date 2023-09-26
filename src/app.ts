bfbc3adfb90789ecb9f6b0a0e06c3ae5e1c76efa
mmarczak-dna <marcin.marczak@digitalnewagency.com>
2023-09-26 11:17:48 +0200

    chore: remove plugins

--- src/app.ts
+++ src/app.ts
@@ -17,15 +17,8 @@ const app: FastifyPluginAsync<AppOptions> = async (
   await fastify.register(require('@fastify/swagger'))
   
   await fastify.register(require('@fastify/swagger-ui'))
-  // Do not touch the following lines
 
-  // This loads all plugins defined in plugins
-  // those should be support plugins that are reused
-  // through your application
-  void fastify.register(AutoLoad, {
-    dir: join(__dirname, 'plugins'),
-    options: opts
-  })
+  // Do not touch the following lines
 
   // This loads all plugins defined in routes
   // define your routes in one of these
